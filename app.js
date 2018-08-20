5dcbac36b545a201540e56d72d0f7bbabb32395b
Sachendra Singh <sachin.s@wrldc.local>
2018-08-20 14:24:53 +0530

    'barebones'

--- app.js
+++ app.js
@@ -14,19 +14,24 @@ app.use(bodyParser.json());
 
 app.use(express.json());
 
-app.post("/echo", function(req, res) {
+app.post("/echo", function (req, res) {
     var speech =
-      req.body.result &&
-      req.body.result.parameters &&
-      req.body.result.parameters.echoText
-        ? req.body.result.parameters.echoText
-        : "Seems like some problem. Speak again.";
+        req.body.queryResult &&
+            req.body.queryResult.queryText
+            ? req.body.queryResult.queryText
+            : "Seems like some problem. Speak again.";
     return res.json({
-      speech: speech,
-      displayText: speech,
-      source: "webhook-echo-sample"
+        fulfillmentText: "This is a text response",
+        source: "webhook-echo-sample"
     });
-  });
+});
+
+app.get("/", function (req, res) {
+    return res.json({
+        fulfillmentText: "This is a text response",
+        source: "webhook-echo-sample"
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => console.log('App listening on port 3000!'));
 
