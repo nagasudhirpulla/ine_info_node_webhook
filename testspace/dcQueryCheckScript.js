f0adb0af0078c36754dc7545c5c79723e9ccd645
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-11-21 12:54:53 +0530

    minor

--- testspace/dcQueryCheckScript.js
+++ testspace/dcQueryCheckScript.js
@@ -2,7 +2,7 @@ var wbesHelper = require('../dbHelpers/wbesHelper');
 
 var queryParams = {
     wbes_entity: ["WR"],
-    wbes_metric : ["urs"]
+    wbes_metric : ["margin"]
 };
 
 wbesHelper.handleWbesQuery(queryParams, function (err, res) {
