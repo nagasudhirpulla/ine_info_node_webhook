fcfe66c9a487a22f5b66a9d53f34fab209b5d74c
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-06-12 12:38:38 +0530

    minor changes in cod info

--- dbHelpers/cod_info.js
+++ dbHelpers/cod_info.js
@@ -1,7 +1,11 @@
 module.exports.cod_dates = {
     "GADARWARA": {
         "unit 1": "2019-05-01"
+    },
+    "SOLAPUR": {
+        "unit 1": "2019-05-01",
+        "unit 2": "2019-05-01"
     }
 }
 
-module.exports.cod_utils = ["GADARWARA"]
+module.exports.cod_utils = ["GADARWARA", "SOLAPUR"]
